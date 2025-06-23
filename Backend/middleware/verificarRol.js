c6304bf3049f1ccffdb05b0162431a11f32375a0
Rodrigo GC <gonzalezrodrigo118@gmail.com>
2025-06-22 22:27:16 -0300

    Update verificarRol.js

--- Backend/middleware/verificarRol.js
+++ Backend/middleware/verificarRol.js
@@ -1,6 +1,7 @@
 const verificarRol = (rolesPermitidos) => {
     return (req, res, next) => {
-        const rol = req.usuario.rol
+        const rol = req.usuario?.rol
+        
         if (!rolesPermitidos.includes(rol)) {
             return res.status(403).json({mensaje: 'Acceso denegado: Rol no autorizado'})
         }
