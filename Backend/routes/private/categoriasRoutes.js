0999e5e253c9ad8ab6f1b88275731f9683ca28a0
Rodrigo GC <gonzalezrodrigo118@gmail.com>
2025-06-23 02:47:49 -0300

    Update categoriasRoutes.js

--- Backend/routes/private/categoriasRoutes.js
+++ Backend/routes/private/categoriasRoutes.js
@@ -1,9 +1,9 @@
-const express = Require('express')
-const outer = express.Router()
-const controlador = Require('../controllers/categoriaController')
+const express = require('express')
+const router = express.Router()
+const controlador = require('../../controllers/categoriaController')
 
 router.get('/', controlador.listarCategorias)
-router.get('/id/:id', controlador.obtenerCategoria)
+router.get('/id/:id', controlador.obtenerCategoriaPorId)
 router.get('/nombre/:nombre', controlador.obtenerCategoriaPorNombre)
 router.post('/', controlador.crearCategoria)
 router.put('/:id', controlador.editarCategoria)
