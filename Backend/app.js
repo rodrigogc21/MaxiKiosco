06cd2a09a538c7eed30d74a31c3d9e772cf6ba68
Rodrigo GC <gonzalezrodrigo118@gmail.com>
2025-06-23 02:36:07 -0300

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,19 @@
-const express = Require('express');
-const cors = Require('cors')
-Require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 // Rutas públicas
-app.use('/api/productos', Require('./routes/public/verProductosRoutes'))
-app.use('/api/auth', Require('./routes/public/authRoutes'))
+app.use('/api/productos', require('./routes/public/verProductosRoutes'))
+app.use('/api/auth', require('./routes/public/authRoutes'))
 
 //Rutas privadas
-app.use('/api/modificar-productos', Require('./routes/private/crudProductosRoutes'))
-app.use('/api/usuarios', Require('./routes/private/usuariosRoutes'))
-app.use('/api/proveedores', Require('./routes/private/proveedoresRoutes'))
-app.use('/api/categorias', Require('./routes/private/categoriasRoutes'))
-app.use('/api/ventas', Require('./routes/private/ventasRoutes'))
-app.use('/api/ordenes-compra', Require('./routes/private/ordenCompraRoutes'))
+app.use('/api/modificar-productos', require('./routes/private/crudProductosRoutes'))
+app.use('/api/usuarios', require('./routes/private/usuariosRoutes'))
+app.use('/api/proveedores', require('./routes/private/proveedoresRoutes'))
+app.use('/api/categorias', require('./routes/private/categoriasRoutes'))
+app.use('/api/ventas', require('./routes/private/ventasRoutes'))
+app.use('/api/ordenes-compra', require('./routes/private/ordenCompraRoutes'))
