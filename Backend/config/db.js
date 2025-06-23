f3bd8d9df2715e530c88136d8bd663db2102e631
Rodrigo GC <gonzalezrodrigo118@gmail.com>
2025-06-23 02:38:41 -0300

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 
 const pool = mysql.createPool ({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    DATABASE: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
