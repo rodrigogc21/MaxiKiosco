37d5f240371fdaab939182996db00f0e243bd231
Rodrigo GC <gonzalezrodrigo118@gmail.com>
2025-06-24 09:04:21 -0300

    Update authRoutes.js

--- Backend/routes/public/authRoutes.js
+++ Backend/routes/public/authRoutes.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const authController = require('../../controllers/authController');
 const usuarioController = require('../../controllers/usuarioController');
 
-// Login
-router.post('/login', authController.login);
 
-// Registro de cliente
+router.post('/login', authController.login);
 router.post('/registro', usuarioController.registrarCliente);
 
 module.exports = router;
