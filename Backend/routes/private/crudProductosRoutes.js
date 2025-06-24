19f189a495810fbedc13484e3f58bddbdb478057
Rodrigo GC <gonzalezrodrigo118@gmail.com>
2025-06-24 09:02:14 -0300

    Update crudProductosRoutes.js

--- Backend/routes/private/crudProductosRoutes.js
+++ Backend/routes/private/crudProductosRoutes.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 const productoController = require('../../controllers/productoController');
 
-// Crear producto
+
 router.post('/', productoController.crearProducto);
-
-// Editar producto
 router.put('/:id', productoController.editarProducto);
-
-// Eliminar producto
 router.delete('/:id', productoController.eliminarProducto);
-
-// Obtener producto por ID
 router.get('/:id', productoController.obtenerProducto);
 
 module.exports = router;
